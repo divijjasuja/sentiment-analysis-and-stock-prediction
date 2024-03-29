59c71b344501c05ad0048b8ea8316b668eb64938
Pranay <pranayraturi9@gmail.com>
2024-03-29 20:05:10 +0530

    download_data jupyter notebook changed. Added all the stock information into folder

--- stock_retriever.py
+++
@@ -1,6 +0,0 @@
-import yfinance as yf
-import pandas as pd
-
-def get_stock_data(name, start_date, end_date):
-    stock_data = yf.download(name, start=start_date, end=end_date)
-    return stock_data
